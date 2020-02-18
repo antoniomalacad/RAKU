b72d6ddd450e1898ee7a1ef3a78a72ea832bb54a
Mary <mary@codechrysalis.io>
2020-02-18 20:17:33 +0900

    created basic Nasa component with daily photo and explanation

--- src/components/Nasa.jsx
+++ src/components/Nasa.jsx
@@ -1,32 +1,27 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-require("dotenv").config({ path: __dirname + "/.env" });
 
 export default function Nasa() {
   const [explanation, setExplanation] = useState("");
   const [image, setImage] = useState("");
 
-  const nasaKey = process.env.REACT_APP_NASA_API_KEY;
-  console.log(nasaKey);
-
   useEffect(() => {
-    const nasaTest = async () => {
-      const results = await axios.get(
-        `https://api.nasa.gov/planetary/apod?api_key=KoKS6MRHHZ5yflOqxhjGQpPiZSt0jgnnF0xuyqJa`
-      );
+    const getNasa = async () => {
+      const results = await axios.get("/api/nasa");
       console.log(results);
+
       setExplanation(results.data.explanation);
       setImage(results.data.url);
     };
-    nasaTest();
-  });
 
-  console.log(explanation);
+    getNasa();
+  }, []);
+
   return (
     <div>
       <h1>Nasa</h1>
       <p>{explanation}</p>
-      <img src={image}></img>
+      <img src={image} alt="starry sky"></img>
     </div>
   );
 }
