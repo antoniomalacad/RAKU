883d04f5d267a440bcb3330ba626731f7da86b98
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-02-19 14:25:12 +0900

    email panel front end

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,13 @@ import Nasa from "./components/Nasa";
 import Weather from "./components/Weather";
 import Quote from "./components/Quote";
 import News from "./components/News";
+import Cats from "./components/Cats";
+import Emails from "./components/Emails";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cats />
       <Quote />
       <Weather />
       <Nasa />
