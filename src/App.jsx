28197ec75c5a59b086302ac7baabe5f98e9f413e
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-02-19 15:19:08 +0900

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Weather from "./components/Weather";
 import Quote from "./components/Quote";
 import News from "./components/News";
 import Emails from "./components/Emails";
+import Cats from "./components/Cats";
 
 function App() {
   return (
