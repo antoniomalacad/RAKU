49e98971bd7d672f0878afc43034cfb0ed35329b
KotaAoyama <hongcha811@gmail.com>
2020-02-19 14:38:29 +0900

    bug fix for front page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Nasa from "./components/Nasa";
 import Weather from "./components/Weather";
 import Quote from "./components/Quote";
 import News from "./components/News";
-import Cats from "./components/Cats";
 import Emails from "./components/Emails";
 
 function App() {
